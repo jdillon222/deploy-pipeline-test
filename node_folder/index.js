2751abd7dcd4531bc86cfd77f7f17f78dedf2d32
jdillon222 <james@plume.com>
2019-02-25 15:08:22 -0800

    removed webhook, this shouldn't work

--- node_folder/index.js
+++ node_folder/index.js
@@ -4,6 +4,6 @@ exports.handler = (event, context, callback) => {
     currentTime.setTimezone("America/Los_Angeles");
     callback(null, {
         statusCode: '200',
-        body: 'The time in Arkansas is: ' + currentTime.toString(),
+        body: 'The time in Wichita,KS is: ' + currentTime.toString(),
     });
 };
